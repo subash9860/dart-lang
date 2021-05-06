eacecca4406b3c3c2fb6d60062846de9b9bdcc4f
subash kc <subaazkc@gmail.com>
2021-05-06 13:21:31 +0545

    first commit

---
+++ null.dart
@@ -0,0 +1,7 @@
+main() {
+  print("Hello tasting null and dynamic");
+  dynamic myNumber;
+  var num;
+  print(myNumber);
+  print(num);
+}
--- variable.dart
+++ variable.dart
@@ -1,22 +1,24 @@
-// this is comment sign
-//Boolean
 void main() {
- bool isOn;
+  bool isOn = false ;
   bool isMan = true;
   print('isOn =${isOn}');
   print('isOn is a ${isOn.runtimeType}');
   print('isOn =${isMan}');
   print('isOn is a ${isMan.runtimeType}');
-
-   // Number
-  num age = 0;
+  
+  // Number
+  int age = 44;
   int people = 6;
   double temp = 30.6;
-
+  
+  print('Age:${age}');
+  print("Pepople:${people}");
+  print("Temp:${temp}");
+  
   int test = int.parse('10');
   print(test);
 
   // testing error
-  int err = int.parse('12.09', onError: (source) => null);
+  int err = int.parse('12.09');
   print('err= ${err}');
 }
