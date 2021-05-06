b2ad50725834ae0f5fb4d6ca1507accfcc6ac532
subash kc <subaazkc@gmail.com>
2021-05-06 13:59:13 +0545

    String of dartLang

---
+++ strings.dart
@@ -0,0 +1,16 @@
+main(List<String> arguments) {
+  String NepaliString = " सुबास ";
+  String englishString = "This is some English text.";
+  print(
+      "Here is some Nepali script -$NepaliString and some english script ${englishString}");
+
+  String concat = 'Subash ' + 'kc';
+  print(concat);
+
+  String multiLine = """
+  This is multiline string
+  and i am creating his 
+  this is same as python
+  """;
+  print(multiLine);
+}
