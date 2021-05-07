fee3ff7d70484b0c512dac7793ef6782611897b1
subash kc <subaazkc@gmail.com>
2021-05-07 14:22:01 +0545

    for loop

---
+++ flowControl.dart
@@ -0,0 +1,11 @@
+main() {
+  int n = 9, m = 10;
+  // if else
+  if (n < m)
+    print('$m is largest number');
+  else
+    print('$n is largest number');
+
+  // conditional statement
+  (n > m) ? print('$n is largest number') : print('$m is largest number');
+}
