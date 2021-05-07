23a439d530dce9fd299eea0ab4fb730d2ee9199d
subash kc <subaazkc@gmail.com>
2021-05-07 15:05:00 +0545

    continue same as c

--- continueinloop.dart
+++ continueinloop.dart
@@ -2,7 +2,7 @@ void loopContinue() {
   for (var num = 1; num <= 5; num++) {
     if (num % 2 == 0) {
       print("These are all even number.$num");
-      continue;
+      continue; // same as c 
     }
     print("These are all odd numbers.$num");
   }
