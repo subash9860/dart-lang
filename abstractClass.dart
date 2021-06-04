bc0ba20d530c19c6f4014d1ae8d9b2ca355df6c6
subash kc <subaazkc@gmail.com>
2021-06-04 11:35:36 +0545

    abs class

--- abstractClass.dart
+++ abstractClass.dart
@@ -14,4 +14,22 @@ class soundSystem extends volume {
   void increase() {
     print("Sound is up.");
   }
+
+  void decrease() {
+    print("Sound is down.");
+  }
+
+  // it is optional to overrife the normal function
+
+  void anyNormalFunc(int age) {
+    print(
+        "This is a normal function to know how old the sound system is :$age");
+  }
+}
+
+main(List<String> arguments) {
+  var newSystem = soundSystem();
+  newSystem.increase();
+  newSystem.decrease();
+  newSystem.anyNormalFunc(10);
 }
